0261933d988874b52fe7e073b41ab9999e158eb1
glanctot <gregory.lanctot28@gmail.com>
2021-12-23 09:43:25 -0600

    first push

---
+++ index.js
@@ -0,0 +1,13 @@
+const inquirer = require('inquirer');
+
+const startingQuestions = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+        }
+    ])
+    .then()
+}
