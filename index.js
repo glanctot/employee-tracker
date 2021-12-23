64de04d685cb7f357075040b9a77f117f6602e94
glanctot <gregory.lanctot28@gmail.com>
2021-12-23 09:50:51 -0600

    brach push

--- index.js
+++ index.js
@@ -12,7 +12,27 @@ const startingQuestions = () => {
     ])
     .then(answer => {
         switch (answer.choice) {
-            
+            case "View all departments":
+                viewAllDepartments();
+                break;
+            case "View all roles":
+                viewAllRoles();
+                break;
+            case "View all employees":
+                viewAllEmployees();
+                break;
+            case "Add a department":
+                addDepartment();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "Add an employee":
+                addEmployee();
+                break;
+            case "Update an employee role":
+                updateEmployeeRole();
+                break;
         }
     })
 }
